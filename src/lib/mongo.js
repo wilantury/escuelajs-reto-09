abfaf6cf7620aa33665a3a83c1cd71385f19bf6b
wilantury <ventas@antury.net>
2019-11-01 16:45:27 -0500

    Problem 2: done

--- src/lib/mongo.js
+++ src/lib/mongo.js
@@ -9,13 +9,13 @@ const MONGO_URI = `mongodb+srv://${USER}:${PASSWORD}@${config.dbHost}:${config.d
 
 class MongoConnect {
   constructor() {
-    this.client = new MongoClient(MONGO_URI, { useNewUrlParser: true });
+    this.client = new MongoClient(MONGO_URI, { useNewUrlParser: true }, { useUnifiedTopology: true });
     this.dbName = DB_NAME;
   }
 
   connect() {
-    if (!MongoLib.connection) {
-      MongoLib.connection = new Promise((resolve, reject) => {
+    if (!MongoConnect.connection) {
+      MongoConnect.connection = new Promise((resolve, reject) => {
         this.client.connect(err => {
           if (err) {
             reject(err);
@@ -25,7 +25,13 @@ class MongoConnect {
         });
       });
     }
-    return MongoLib.connection;
+    return MongoConnect.connection;
+  }
+
+  getAll(collection) {
+    return this.connect().then(db => {
+      return db.collection(collection);
+    })
   }
 }
 
--- src/services/index.js
+++ src/services/index.js
@@ -1,8 +1,15 @@
 const { productsMock } = require('../utils/mocks');
+const MongoConnect = require('../lib/mongo');
 
 class ProductService {
+
+  constructor(){
+    this.collection = 'products';
+    this.mongoDb =  new MongoConnect();
+  }
+
   async getProducts() {
-    const products = await Promise.resolve(productsMock);
+    const products = await this.mongoDb.getAll(this.collection);//Promise.resolve(productsMock);//
     return products || [];
   }
 }
