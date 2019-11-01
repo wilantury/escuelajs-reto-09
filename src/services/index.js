5d49ff95576deaf7fcad8365b47f6094f1a4971c
wilantury <ventas@antury.net>
2019-11-01 16:52:06 -0500

    Problem 3.12: done

--- src/services/index.js
+++ src/services/index.js
@@ -12,6 +12,26 @@ class ProductService {
     const products = await this.mongoDb.getAll(this.collection);//Promise.resolve(productsMock);//
     return products || [];
   }
+
+  async getProduct({ productId }){
+    const product = await this.mongoDb.get(this.collection, productId);
+    return product || {};
+  }
+
+  async createProduct({ product }){
+    const productCreatedId = await this.mongoDb.create(this.collection, product);
+    return productCreatedId;
+  }
+
+  async updateProduct({ productId, product } = {}){
+    const productUpdatedId = await this.mongoDb.update(this.collection, productId, product);
+    return productUpdatedId;
+  }
+
+  async deleteProduct({ productId }){
+    const productDeletedId = this.mongoDb.delete(this.collection, productId);
+    return productDeletedId;
+  }
 }
 
 module.exports = ProductService;
